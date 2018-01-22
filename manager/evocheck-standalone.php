a3decb3f69578c7f3e1d88f747918affc25a9bbb
Deesen <dh@fuseit.de>
2018-01-22 14:41:14 +0100

    Moved evocheck-standalone.php into root-dir - https://github.com/extras-evolution/evocheck/pull/8/

--- manager/evocheck-standalone.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('IN_MANAGER_MODE', 'true');
-require('includes/config.inc.php');
-$standaloneFile = basename(__FILE__);
-startCMSSession();
-require(MODX_BASE_PATH.'assets/modules/evocheck/evocheck.module.php');
